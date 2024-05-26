957a6852b9566f64bd6c75b154e93e0252f6f851
teemonaco <teemonaco@gmail.com>
2024-05-26 16:06:05 -0700

    latest updates new gallery

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,5 @@
 
 $(document).ready(function() {
-    $('#fullPage').fullpage({
-        verticalCentered: false,
-        scrollingSpeed: 600,
-        autoScrolling: true,
-        css3: true,
-        navigation: true,
-        navigationPosition: 'right',
-        paddingTop: '50px' // Adjust based on the actual height of your header
-    });
-});
-
-$(document).ready(function() {
     lightbox.option({
       'resizeDuration': 200,
       'wrapAround': true
